f67a0334afe020867df8eb2dd30287a40970b2bc
nailseong <nnis9857@gmail.com>
2021-01-16 19:37:37 +0900

    CreateUser UnitTest

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -6,6 +6,8 @@ import { UserService } from './user.service';
 
 const mockRepo = () => ({
   findOne: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
 });
 
 type MockRepo<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
@@ -29,7 +31,61 @@ describe('User Service', () => {
     userRepo = moduleRef.get(getRepositoryToken(User));
   });
 
-  it('be defined', () => {
-    expect(userService).toBeDefined();
+  describe('createUser', () => {
+    const existUser = {
+      username: 'existUsername',
+      email: 'existEmail',
+    };
+    const newUser = {
+      username: 'newUsername',
+      email: 'newEmail',
+      password: 'newPassword',
+    };
+
+    it('Error : Already exist username', async () => {
+      userRepo.findOne.mockResolvedValue(existUser);
+
+      const result = await userService.createUser({
+        ...newUser,
+        username: existUser.username,
+      });
+      expect(result).toEqual({
+        error: 'Already exist username',
+      });
+    });
+
+    it('Error : Already exist email', async () => {
+      userRepo.findOne.mockResolvedValueOnce(null);
+      userRepo.findOne.mockResolvedValue(existUser);
+
+      const result = await userService.createUser({
+        ...newUser,
+        email: existUser.email,
+      });
+      expect(result).toEqual({
+        error: 'Already exist email',
+      });
+    });
+
+    it('Error : Unexpected error', async () => {
+      userRepo.findOne.mockRejectedValue(new Error());
+
+      const result = await userService.createUser(newUser);
+      expect(result).toEqual({
+        error: 'Unexpected error',
+      });
+    });
+
+    it('Create new user', async () => {
+      userRepo.findOne.mockResolvedValue(null);
+      userRepo.findOne.mockResolvedValue(null);
+
+      const result = await userService.createUser(newUser);
+      expect(result).toEqual({
+        ok: true,
+      });
+      expect(userRepo.save).toBeCalled();
+      expect(userRepo.create).toBeCalledWith(newUser);
+    });
   });
 });
