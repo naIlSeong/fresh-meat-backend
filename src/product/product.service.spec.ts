d2e194d444c5d8ab2a56d3871c79007d658a7cd0
nailseong <nnis9857@gmail.com>
2021-01-19 01:20:20 +0900

    UploadProduct UnitTest

--- src/product/product.service.spec.ts
+++ src/product/product.service.spec.ts
@@ -1,11 +1,12 @@
 import { Test } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from 'src/user/user.entity';
 import { Repository } from 'typeorm';
 import { Product } from './product.entity';
 import { ProductService } from './product.service';
 
 const mockRepo = () => ({
-  findOne: jest.fn(),
+  create: jest.fn(),
 });
 
 type MockRepo<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
@@ -13,6 +14,8 @@ type MockRepo<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 describe('ProductService', () => {
   let productService: ProductService;
   let productRepo: MockRepo<Product>;
+  let mockUser: User;
+  let mockProduct: Product;
 
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -27,9 +30,40 @@ describe('ProductService', () => {
 
     productService = moduleRef.get<ProductService>(ProductService);
     productRepo = moduleRef.get(getRepositoryToken(Product));
+
+    mockUser = new User();
+    mockUser.id = 3;
+
+    mockProduct = new Product();
+    mockProduct.id = 7;
+    mockProduct.seller = mockUser;
+    mockProduct.productName = 'mockProductName';
+    mockProduct.startPrice = 1000;
   });
 
-  it('works?', () => {
-    expect(productService).toBeDefined();
+  describe('uploadProduct', () => {
+    it('Error : Unexpected error', async () => {
+      productRepo.create.mockRejectedValue(new Error());
+
+      const result = await productService.uploadProduct({
+        productName: mockProduct.productName,
+        startPrice: mockProduct.startPrice,
+      });
+      expect(result).toEqual({
+        error: 'Unexpected error',
+      });
+    });
+
+    it('Upload new product', async () => {
+      const result = await productService.uploadProduct({
+        productName: mockProduct.productName,
+        startPrice: mockProduct.startPrice,
+      });
+      expect(result).toEqual({
+        ok: true,
+      });
+      expect(productRepo.create).toBeCalled();
+      expect(productRepo.save).toBeCalled();
+    });
   });
 });
